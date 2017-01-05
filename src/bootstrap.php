024f29334cd794855c4a6c9dd5045d77f4263b0c
David Dan <davidwdan@gmail.com>
2017-01-05 13:30:22 -0500

    Updated to react-async-interop-loop v 0.2.1

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 
 use Interop\Async\Loop;
-use React\EventLoop\StreamSelectLoop;
 use WyriHaximus\React\AsyncInteropLoop\ReactDriverFactory;
 
-Loop::setFactory(ReactDriverFactory::createFactoryFromLoop(StreamSelectLoop::class));
+Loop::setFactory(ReactDriverFactory::createFactory());
 
 register_shutdown_function(function () {
     Loop::execute(function () {}, Loop::get());
